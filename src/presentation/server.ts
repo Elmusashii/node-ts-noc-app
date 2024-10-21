b2da20b0ceb362fa5c40769ba3a80e4e25e650d9
carlos <hredx708@gmail.com>
2024-10-21 13:42:34 -0300

    fin seccion 8

---
+++ src/presentation/server.ts
@@ -0,0 +1,21 @@
+import { CronService } from "./cron/cron.service";
+import { CheckService } from "./domain/use-case/checks/check-service";
+
+export class Server {
+    public static start() {
+        console.log('Server is running...');
+        CronService.createJob(
+            '*/5 * * * * *',
+            ()=>{
+                const url = 'https://www.google.com'; 
+                new CheckService(
+                    ()=>{console.log(`${url} is ok...`)},
+                    (error)=>{console.log(error)}
+                ).execute(url)
+                //new CheckService().execute('http://localhost:3000')
+            }
+        );
+
+    }
+}
+
